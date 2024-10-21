a938894ec8c7c71dad1a838e17133e55fd910612
DSTV <9871@holbertonstudents.com>
2024-10-21 02:34:34 +0000

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point 
+ * Description: Printing a message using the puts() function
+ * Return: Always 0 (success) 
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
