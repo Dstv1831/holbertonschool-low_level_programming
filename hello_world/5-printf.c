51c76b1d08a89e04907e529d7105499c0aad5507
DSTV <9871@holbertonstudents.com>
2024-10-21 04:43:55 +0000

    Task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print a text using the printf() function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
