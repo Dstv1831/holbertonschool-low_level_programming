dcf6d13fc05ab13120ec7455a37772c5be8a4b09
DSTV <9871@holbertonstudents.com>
2024-10-22 23:25:07 +0000

    Task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - function that checks if a number
+ * is positive, negative or value 0.
+ * @c: Number entered by user.
+ *
+ * Return: 1,0 or -1 and +,0,- depending the input value.
+ */
+
+int print_sign(int c)
+{
+	if (c < 0)
+		return (-1);
+	else if (c > 0)
+		return (1);
+	else
+		return (0);
+}
+
