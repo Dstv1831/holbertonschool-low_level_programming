8aaac79b761e97f3170ce57888e2622378b8a6d1
DSTV <9871@holbertonstudents.com>
2024-10-22 01:41:22 +0000

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase with
+ * the putchar() function
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
