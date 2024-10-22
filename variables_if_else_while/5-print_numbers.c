2d9c975d625b175ac27266e3a6181c43a651f955
DSTV <9871@holbertonstudents.com>
2024-10-22 01:01:07 +0000

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers
+ * of base 10 starting from 0
+ *
+ * Return: Alaways 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (i = 0; i < 10; i++)
+		printf(i);
+	print('\n');
+
+	return (0);
+}
