d3916b02312680b16b1c30bc5628ab319790a7f4
DSTV <9871@holbertonstudents.com>
2024-10-22 14:22:33 +0000

    Task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic characters
+ * regardless if they are lowercase or UPPERCASE
+ * @c: Key entered by user.
+ *
+ * Return: 1 if c is an alphabetic character, 0 if not.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'B'))
+		return (1);
+	else
+		return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+/**
+ * File - Header file conatining 
+ * forward declaration for all the functions
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
