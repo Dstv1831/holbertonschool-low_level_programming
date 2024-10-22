b0cbd29891d310316ebb22aa55d40f81caf9589d
DSTV <9871@holbertonstudents.com>
2024-10-22 23:48:13 +0000

    Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - function that checks the absolute
+ * value of a number.
+ * @c: Number entered by user.
+ *
+ * Return: Absoulte value of the number entered.
+ */
+
+int _abs(int c)
+{
+	int abs;
+
+	abs = c * c;
+	return (abs);
+}
+
