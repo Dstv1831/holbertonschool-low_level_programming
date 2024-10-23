ab5a6302b9bf790d50d46dbdefcf18f1503a23a5
DSTV <9871@holbertonstudents.com>
2024-10-23 06:31:08 +0000

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,27 +9,17 @@
 
 void print_to_98(int n)
 {
-	int i;
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 
-	if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-	}
-	else if (n < 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
 	}
 	else
-		printf("%d", n);
-	printf("\n");
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
