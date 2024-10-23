5ff0cc49cd1193f2d566194dfa195a1a8e25a48d
DSTV <9871@holbertonstudents.com>
2024-10-23 00:39:37 +0000

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @c: Number entered by user.
+ *
+ * Return: Value of the last digit in a number
+ */
+
+int print_last_digit(int c)
+{
+	int ld;
+
+	ld = c % 10;
+
+	return (ld);
+
+}
