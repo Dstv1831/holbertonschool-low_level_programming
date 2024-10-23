568ddd2c24f51f01a28f75eb384d1d420390e392
DSTV <9871@holbertonstudents.com>
2024-10-23 05:55:48 +0000

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 			if (i != 98)
-				printf(", ")
+				printf(", ");
 			printf("%d", i);
 	}
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 			if (i != 98)
-				printf(", ")
+				printf(", ");
 			printf("%d", i);
 	}
 	else
