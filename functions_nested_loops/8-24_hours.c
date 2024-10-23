36e92ca91ca772575331e3a8d3df17e8f80e61c1
DSTV <9871@holbertonstudents.com>
2024-10-23 02:28:48 +0000

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,38 +10,23 @@
 
 void jack_bauer(void)
 {
-	int s, m, h, H;
+	int m, h;
 
-	s = 0;
 	m = 0;
 	h = 0;
-	H = 0;
 	
-	while (H < 2)
+	while (h < 23)
 	{
-		while (h < 10)
+		while (m < 60)
 		{
-			while (m < 6)
-			{
-				while (s < 10)
-				{
-					_putchar('0' + H);
-					_putchar('0' + h);
-					_putchar(':');
-					_putchar('0' + m);
-					_putchar('0' + s);
-					_putchar('\n');
-					s++;
-				}
-				m++;
-				s = 0;
-			}
-			h++;
-			m = 0;
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+			m++;
 		}
-		H++;
-		h = 0;
+		h++;
 	}
-	H = 0;
-
 }
