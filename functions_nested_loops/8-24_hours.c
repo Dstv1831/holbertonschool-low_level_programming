eec76455f2af8cc296fe09b3b99220579d9e0c42
DSTV <9871@holbertonstudents.com>
2024-10-23 01:53:06 +0000

    Task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute
+ * of the day, starting from
+ * 00:00 to 23:59.
+ *
+ * Return: Every minute in a new line.
+ */
+
+void jack_bauer(void)
+{
+	int s, m;
+
+	s = 0;
+	m = 0;
+	h = 0;
+	H = 0;
+
+	while (H < 2)
+	{
+		_putchar('0' + H);
+		while (h < 9)
+		{
+			_putchar('0' + h);
+			while (m < 60)
+			{
+				_putchar('0' + m);
+				while (s < 60)
+				{
+					_putchar('0' + s);
+					_putchar('\n');
+					s++;
+				}
+				m++;
+			}
+			h++;
+		}
+		H++;
+	}
+}
