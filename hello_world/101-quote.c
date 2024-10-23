48e26f4addef4b9a3f327027107ad056c917a661
DSTV <9871@holbertonstudents.com>
2024-10-23 21:42:37 +0000

    Task 9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 
+/**
+ * main - Prints the message on the Standard Error
+ *
+ * Return: 1 Succes
+ */
+
 int main(void)
 {
-	char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (2, buffer, 59);
+	char *buffer = "and that piece of art is useful\""
+	 + "- Dora Korpar, 2015-10-19\n";
+
+	write(2, buffer, 59);
 	return (1);
 }
