913a4ded433a6bf6bc9541feaf8808af791febe7
DSTV <9871@holbertonstudents.com>
2024-10-23 03:31:40 +0000

    Task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 times table
+ * starting form 0
+ *
+ * Return: Every minute in a new line.
+ */
+
+void times_table(void)
+{
+	int i, j, ans;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			ans = i * j;
+			_putchar(ans);
+		}
+	}
+}
