69e1f451a100d5338ed2f7e1518023ce3f0ed7dd
DSTV <9871@holbertonstudents.com>
2024-10-28 01:34:24 +0000

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	j = 0;
+
+	while (j < 9)
+	{
+		for (i = 0; i <= 14; i++)
+			_putchar('0' + i);
+		_putchar('\n');
+		j++;
+	}
+	_putchar('\n');
+}
