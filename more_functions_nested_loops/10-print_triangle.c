57a72c0d250e970d71f2aee27b7859bf3a607430
DSTV <9871@holbertonstudents.com>
2024-10-28 05:55:58 +0000

    Task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Draws a triangle,
+ * using the character '#'.
+ * @size: Size of the triangle.
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size; j > 0; j--)
+			{
+				if (j == i)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
