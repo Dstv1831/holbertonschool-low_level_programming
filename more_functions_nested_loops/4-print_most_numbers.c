520feeb81bec5c743e5cb37dd81f181e62b2e857
DSTV <9871@holbertonstudents.com>
2024-10-28 01:26:40 +0000

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * except for 2 & 4, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar('0' + i);
+	_putchar('\n');
+}
