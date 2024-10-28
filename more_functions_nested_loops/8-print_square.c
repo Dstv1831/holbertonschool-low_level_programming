74a97d409fe37f0517976ea60f598f6255760dc2
DSTV <9871@holbertonstudents.com>
2024-10-28 03:04:25 +0000

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - Draws a square,
+ * using the character '#'.
+ * @size: Size of the square.
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
