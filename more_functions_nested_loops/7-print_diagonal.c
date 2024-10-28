09cc5abb7ac1b451a9f68bd7d47901d12be0d456
DSTV <9871@holbertonstudents.com>
2024-10-28 02:32:47 +0000

    Task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line in the terminal
+ * with its lenght base on the value of n.
+ * @n: number of timers the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+			_putchat('\n');
+		}
+	_putchar('\n');
+}
