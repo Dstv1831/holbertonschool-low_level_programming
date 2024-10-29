ba02fe66d69cfb52da7547665085af728c99a753
DSTV <9871@holbertonstudents.com>
2024-10-29 06:25:35 +0000

    Task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string,
+ * followed by a new line, to stdout.
+ * @str: String parameter.
+ */
+
+void _puts(char *str)
+{
+	int lenght = 0;
+
+	/**
+	 * this condition (*str) is iqual to (*str = '\0')
+	 * where '\0' is the last value of every string
+	 * also '\0' in ASCII is of value 0
+	 */
+	while (*str++)
+		_putchar(*str++);
+	_putchar('\n');
+}
