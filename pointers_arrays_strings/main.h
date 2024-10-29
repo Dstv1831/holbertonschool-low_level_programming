2b4196964a0e1d1d17787be7ea96bd6563018894
DSTV <9871@holbertonstudents.com>
2024-10-29 11:59:30 +0000

    Task 3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,8 @@
  * File - Header file containing 
  * forward declaration for all the functions
  */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
