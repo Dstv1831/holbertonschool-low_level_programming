f6944d04daa9ac8cec18360b55e7fb94fe5ce1e8
DSTV <9871@holbertonstudents.com>
2024-10-30 02:55:44 +0000

    Task Completed

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,8 +4,8 @@
  * _strcpy - function that copies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest.
- * @dest: String parameter.
- * @src: number of elements to be printed.
+ * @dest: A buffer to copy the String to.
+ * @src: Source of the string to copy.
  *
  * Return: pointer to dest
  */
