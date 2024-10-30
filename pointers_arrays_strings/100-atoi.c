9ad9bb29353b8e6fb76f1656ad68ab82bd90f308
DSTV <9871@holbertonstudents.com>
2024-10-30 06:45:29 +0000

    Task Completed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,30 +3,29 @@
 
 /**
  * _atoi - function that converts string to integer.
- * 
+ *
  * @s: String that will be converted.
  *
  * Return: Integer value
  */
 
 /*
- * (*s - '0') - Takes the value of s* from asci to decimal 
+ * (*s - '0') - Takes the value of s* from asci to decimal
  * by sustracting '0' or 48
  */
 int _atoi(char *s)
 {
-	int sign = 1, 
+	int sign = 1,
 	unsigned int number = 0;
-	do
-	{
+
+	do {
 		if (*s == '-')
 			sign *= -1;
-		else if ( *s >= '0' && *s <= '9')
+		else if (*s >= '0' && *s <= '9')
 			number = (number * 10) + (*s - '0');
 		else if (number > 0)
 			break;
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (number * sign);
 }
