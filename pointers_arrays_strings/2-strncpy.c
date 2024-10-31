c22b9df05311a6132ec788ec3f7e3e11218df5f7
Dstv <9871@holbertonstudents.com>
2024-10-31 14:44:17 +0000

    Task Completed

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int  j = 0;
+	int  j = 0, src_length;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
-	dest[j] = '\0';
+
+	src_length = j;
+
+	for (j = src_length; j < n; j++)
+		dest[j] = '\0';
 
 	return (dest);
 }
