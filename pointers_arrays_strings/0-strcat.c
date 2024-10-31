e2a8d7b10d12927274f2cc69f31b1211b54ecd15
DSTV <9871@holbertonstudents.com>
2024-10-31 05:00:18 +0000

    Task Completed

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,25 +6,23 @@
  * overwriting the terminating null byte (\0) on the second string,
  * and then adds a new terminating null byte.
  *
- * @*char: First string.
- * @*dest: String Destination to be overwritten.
+ * @src: First string.
+ * @dest: String destination to be overwritten.
  *
  * Return: Pointer to the resulting string "dest"
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int  i = 0, j=0, length = 0;
-	
+	int  i = 0, j = 0, length = 0;
+
 	while (dest[i++])
-		length ++;
-	
-	/*for (i = 0; src[i] != '\0'; i++)
-		dest[length + i] = src[i];*/
+		length++;
+
 	while (src[j] != '\0')
 	{
 		dest[length + j] = src[j];
 		j++;
-	}			
+	}
 	return (dest);
 }
