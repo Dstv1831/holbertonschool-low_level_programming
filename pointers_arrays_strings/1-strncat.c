d7330a0ed9f1a5344549a1818f522812b407f5f2
DSTV <9871@holbertonstudents.com>
2024-10-31 12:54:43 +0000

    Task Completed

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - Function that appends two strings,
+ * adding the amount of characters specified on the parameters
+ *
+ * @src: First string.
+ * @dest: String destination to be overwritten.
+ * @n: Number of characters to append
+ *
+ * Return: Pointer to the resulting string "dest"
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int  i = 0, j = 0, length = 0, srcsize = 0;
+
+	while (dest[i++])
+		length++;
+
+	i = 0;
+
+	while (src[i++])
+		srcsize++;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[length + j] = src[j];
+
+	return (dest);
+}
