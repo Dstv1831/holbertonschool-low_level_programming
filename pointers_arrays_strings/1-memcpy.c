a0af734b8a91e1e4b08827212c63b06e85d3ae3f
DSTV <9871@holbertonstudents.com>
2024-11-04 07:55:40 +0000

    Task Completed

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,13 +12,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
-	{
-		*dest++ = *src++;
-		i++;
-	}
-
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
