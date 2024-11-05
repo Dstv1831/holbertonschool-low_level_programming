38aa9d2bf592ff9aefded7c995f4c182d2345fcc
DSTV <9871@holbertonstudents.com>
2024-11-05 13:39:54 +0000

    Task completed

---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+Learning C - Part 6
---
+++ recursion/main.h
@@ -0,0 +1,7 @@
+/**
+ * File - Header file containing 
+ * forward declaration for all the functions
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
