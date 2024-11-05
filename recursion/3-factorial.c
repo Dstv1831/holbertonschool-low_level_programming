40c1fae667b7aa9c830a7dc4787f13c396f9208a
DSTV <9871@holbertonstudents.com>
2024-11-05 23:30:14 +0000

    Task completed

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - Functions that returns
+ * the factorial of a given number
+ *
+ * @n: Integer to work on. 
+ *
+ * Return: Result integer of the factorial of "n"
+ */
+
+int factorial(int n)
+{
+	int res = 0;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return 1;
+	res = n * factorial(n-1);
+	return (res);
+}
