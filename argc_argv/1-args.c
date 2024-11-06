9703a3f79ad5edff5c3da9ef0bab15ed353c5d60
DSTV <9871@holbertonstudents.com>
2024-11-06 22:41:32 +0000

    Task completed

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Return the number of arguments 
+ * passed to the program
+ *
+ * @argv: Vector of strings (program arguments)
+ * @argc: Argument counter
+ *
+ * Return: success 0
+ */
+
+int main(int argc, char *argv[])
+{
+	/* Supress the variable argv from compiling */
+	(void) argv;
+
+	printf("%d\n", argc-1);
+	return (0);
+}
+
