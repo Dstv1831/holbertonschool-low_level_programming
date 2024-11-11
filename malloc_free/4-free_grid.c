10e0b175e35e5533734a827d4cbff8a41b77d5e6
DSTV <9871@holbertonstudents.com>
2024-11-11 07:02:00 +0000

    Task completed

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -14,12 +14,7 @@ void free_grid(int **grid, int height)
 {
 	int index;
 
-	if (grid == NULL)
-		printf("OK\n");
-	else
-	{
-		for (index = 0; index <= height; index++)
+		for (index = 0; index < height; index++)
 			free(grid[index]);
 		free(grid);
-	}
 }
