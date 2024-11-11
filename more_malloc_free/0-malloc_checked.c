0defd18c632802611852490da0e4f2a7c78fbf2e
DSTV <9871@holbertonstudents.com>
2024-11-11 14:37:24 +0000

    Task completed

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ *
+ * @b: Factor of the Size of the memory to allocate.
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
