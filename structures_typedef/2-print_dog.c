9ef600f97d7baed3ae17b71d59009ef830c6579b
DSTV <9871@holbertonstudents.com>
2024-11-14 02:23:10 +0000

    Task completed

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Write a function that prints a struct dog
+ *
+ * @d: dog structure
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		/* Either way is ok "(*d)." and "d->" */
+
+		printf("Name: %s", (*d).name);
+		printf("Age: %d", d->age);
+		printf("Owner: %s", d->owner);
+	}
+	else
+		return (0);
+}
