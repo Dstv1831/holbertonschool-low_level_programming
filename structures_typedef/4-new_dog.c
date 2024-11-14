81424a309d08fa8c90dec1e32aea9328fae4130d
DSTV <9871@holbertonstudents.com>
2024-11-14 08:15:57 +0000

    Task completed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,26 +3,86 @@
 #include <stdlib.h>
 
 /**
+ * len - Function that return the len of a string.
+ *
+ * @string: String to be measured
+ *
+ * Return: length of the string (not including '\0')
+ *
+ */
+
+int len(char string)
+{
+	int len = 0;
+
+	while (*string++ != '\0')
+		len++;
+
+	return (len);
+
+}
+
+/**
+ * copy - Function that copies the string source into another string
+ *
+ * @str_src: Source string
+ * @str_des: Destiny string
+ *
+ * Return: New string copied
+ *
+ */
+
+char copy(char str_src, char str_des)
+{
+	int i;
+
+	for (i = 0; str_src; i++)
+		str_des[i] = str_src[i];
+
+	str[i] == '\0';
+
+	return (str_des);
+
+}
+
+/**
  * new_dog - Function that creates a new dog.
  *
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  *
+ * Return: New dog structure
+ *
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *luna;
+	dog_t *new_dog;
 
-	luna = malloc(sizeof(dog_t));
-	
-	if (luna == NULL)
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
 		return (NULL);
 
-	luna->name = name;
-	luna->age = age;
-	luna->owner = owner;
-	
+	new_dog->name = malloc(len(name) + 1);
+
+	if (new_dog->name == NULL)
+		return (NULL);
+
+	new_dog->owner = malloc(len(owner) + 1);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+
+		return (NULL);
+	}
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
 	return (luna);
 }
