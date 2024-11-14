de0247d2a2eee524e818bbf62743c290b9624c66
DSTV <9871@holbertonstudents.com>
2024-11-14 01:25:03 +0000

    Task completed

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Create a struct of type dog
+ *
+ * @name: Name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ *
+ */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
+#endif /*DOG_H*/
 
