d132c7c0e2e6b2e2315aab1b17d1d7bebbacdae1
DSTV <9871@holbertonstudents.com>
2024-11-18 00:47:19 +0000

    Task Completed

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ *
+ * @name: Mame to print
+ * @f: Pointer to character
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
