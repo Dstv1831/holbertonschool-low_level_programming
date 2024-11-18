6cdc546686f451cb7f6caf2cef545f639fc488b5
DSTV <9871@holbertonstudents.com>
2024-11-18 00:57:33 +0000

    Task Completed

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
+#ifndef FUNPOINTERS_H
+#define FUNPOINTERS_H
+
 /**
- * File - Header file containing 
+ * File - Header file containing
  * forward declaration for all the functions
  */
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+#endif
