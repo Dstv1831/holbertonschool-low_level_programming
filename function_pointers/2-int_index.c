ac689a0ed1fbd6ac42882cb24a5b7768884911f7
DSTV <9871@holbertonstudents.com>
2024-11-19 00:49:42 +0000

    Task Completed

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, size_t size, int (*cmp)(int))
 {
 	unsigned long int i = 0;
 
-	if (size <= 0 || array == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	while (i < size)
