3318a367e196f329ff7cb2c48a7081f743e5a16e
DSTV <9871@holbertonstudents.com>
2024-11-19 00:44:10 +0000

    Task Completed

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches
+ * for an integer.
+ *
+ * @array: pointer to an array
+ * @size: Size of the array
+ * @cmp: pointer to the function
+ *
+ * Return: The index of the first element
+ * for which the "cmp" function does not return 0
+ */
+
+int int_index(int *array, size_t size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		else
+			return (-1);
+		i++;
+	}
+}
--- function_pointers/function_pointer.h
+++
@@ -1,6 +0,0 @@
-/**
- * File - Header file containing 
- * forward declaration for all the functions
- */
-
-int _putchar(char c);
