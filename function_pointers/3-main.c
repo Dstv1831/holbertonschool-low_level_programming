ff447959139ba8942942b91941deff6d99e320c9
DSTV <9871@holbertonstudents.com>
2024-11-19 06:43:10 +0000

    Task Completed

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -27,11 +27,10 @@ int main(int argc, char *argv[])
 		printf("%s\n", msg);
 		exit(98);
 	}
-	else if (get_op_func(operator) == NULL)
+	else if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("%s\n", msg);
-                exit(99);
-
+		exit(99);
 	}
 	else if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
 	{
