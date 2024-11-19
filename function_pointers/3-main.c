4954fe9504e3a9007c0e16c96cddc8195a479505
DSTV <9871@holbertonstudents.com>
2024-11-19 06:52:04 +0000

    Task Completed

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,30 +18,30 @@ int main(int argc, char *argv[])
 	int  num1, num2, res = 0;
 	char *operator, msg[] = "Error";
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	operator = argv[2];
-
 	if (argc != 4)
 	{
 		printf("%s\n", msg);
 		exit(98);
 	}
-	else if (get_op_func(operator) == NULL || operator[1] != '\0')
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("%s\n", msg);
 		exit(99);
 	}
-	else if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
 	{
 		printf("%s\n", msg);
 		exit(100);
 	}
-	else
-	{
-		res = get_op_func(operator)(num1, num2);
-		printf("%d\n", res);
-	}
+
+	res = get_op_func(operator)(num1, num2);
+	printf("%d\n", res);
 
 	return (0);
 }
