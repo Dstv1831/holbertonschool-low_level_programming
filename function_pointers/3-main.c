49e97211aaddfddc7cce8a5ca9ed90b51229f36e
DSTV <9871@holbertonstudents.com>
2024-11-19 06:09:35 +0000

    Task Completed

---
+++ function_pointers/3-main.c
@@ -0,0 +1,51 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "3-calc.h"
+
+/**
+ * main - Prints the result of the operation
+ * that was specified on the command line
+ *
+ * @argv: Vector of strings (program arguments)
+ * @argc: Argument counter
+ *
+ * Return: 0 Succes
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int  num1, num2, res = 0;
+	char *operator, msg = "Error";
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (argc != 4)
+	{
+		printf("%s\n", msg);
+		exit(98);
+	}
+	else if ((operator == '/' && num2 == 0) || (operator == '%' && num2 == 0))
+	{
+		printf("%s\n", msg);
+		exit(100);
+	}
+	else
+	{
+		res = ((*get_op_func(char *operator))(num1, num2));
+		printf("%d\n", res);
+	}
+}
+
+
+
+
+
+
+
+
+	return (0);
+}
+
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,15 +2,14 @@
 #define FUNPOINTERS_H
 
 /**
- * print_name - Prints a name
- *
- * @name: Name to print
- * @f: pointer to function to call
+ * forward declaration of the functions to use 
+ * when including the header file
  *
  */
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, size_t size, int (*cmp)(int));
+
 
 #endif
