19d7381e467c357d789275f45a0839bacca310be
DSTV <9871@holbertonstudents.com>
2024-11-20 00:56:05 +0000

    Task Completed

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - print numbers followed by a new line
- * 
+ *
  * @separator: string to be printed between numbers
  * n: number of integers passed to the function
  *
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(arg_list, int));
 		if (i != n && separator != NULL)
-			printf("%s",separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 
