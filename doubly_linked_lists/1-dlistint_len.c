fdd5119142fd809302511cb64a9eb755fdff6228
DSTV <9871@holbertonstudents.com>
2025-01-09 08:25:25 +0000

    Task Completed

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - calculate the number of
+ * elements in a linked dlist_t list
+ * @h: Pointer to the list_t list.
+ *
+ * Return: Number of elements in h.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
