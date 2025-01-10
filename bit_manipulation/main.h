163e9ec7db60658cf80715db4ca25b37987df9e2
DSTV <9871@holbertonstudents.com>
2025-01-10 05:57:12 +0000

    Task Complete

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,13 +1,14 @@
 #ifndef BIT_H
 #define BIT_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * forward declaration of the functions to use 
  * when including the header file
+ *
  */
 
 unsigned int binary_to_uint(const char *b);
 
-#endif /* _BIT_H */
+#endif
