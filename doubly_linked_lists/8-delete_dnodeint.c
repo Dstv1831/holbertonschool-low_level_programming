f551f6e268823d2018aecb51dce440577a786624
DSTV <9871@holbertonstudents.com>
2025-01-10 01:20:24 +0000

    Task Completed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,10 +12,10 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
-	
+
 	if (*head == NULL)
 		return (-1);
-	
+
 	for (; index != 0; index--)
 	{
 		if (tmp == NULL)
