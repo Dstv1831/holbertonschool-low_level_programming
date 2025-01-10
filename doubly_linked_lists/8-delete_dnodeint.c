8b971ae912a4e18d27456308208dc4b9c3e2b2d4
DSTV <9871@holbertonstudents.com>
2025-01-10 01:15:19 +0000

    Task Completed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int i;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	/* Traverse to the node before idx */
