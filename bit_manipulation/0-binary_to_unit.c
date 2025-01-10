b3cfb4b1969666565f4ff98fa6aeedfc76a09849
DSTV <9871@holbertonstudents.com>
2025-01-10 01:43:19 +0000

    Task Complete

---
+++ bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: Pointing to a string of 0 and 1 chars
+ *
+ * Return: Value of the unsigned int, otherwise NULL
+ */
+
+
+unsigned int binary_to_uint(const char *b);
---
+++ bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef BIT_H
+#define BIT_H
+
+/**
+ * File - Header file containing
+ * forward declaration for all the functions
+ */
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _BIT_H */
