7fbae6fd8b02d45dd904ac00a15ef9e0f9dfeab0
dstv <9871@holbertonstudents.com>
2025-01-13 03:42:12 +0000

    Task Complete

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -12,7 +12,6 @@
  * Returns 0 if b is NULL. or the string contains
  * any character other than '0' or '1'.
  */
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
